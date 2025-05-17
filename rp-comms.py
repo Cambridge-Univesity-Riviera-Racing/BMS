0e96c89fc50a0c4e6eb8146eccb8eb40e551975f
hl638 <124627162+hl638@users.noreply.github.com>
2025-05-17 13:24:46 +0100

    Add files via upload
    Code for reading data from DalyBMS (still need to work on code for raspberry-pi communication)

---
+++ rp-comms.py
@@ -0,0 +1,12 @@
+#CU Riviera Society
+#DALY BMS Tool V1.0.0 and Raspberry Pi Communication
+
+# Raspberry Pi must read parameters and measurements 
+# of DALY BMS Tool V1.0.0
+# Raspberry Pi data must be communicated via 4G connection
+
+import serial
+from daly_bms import DalyBMS
+
+
+DalyBMS.get_all(self=DalyBMS)
